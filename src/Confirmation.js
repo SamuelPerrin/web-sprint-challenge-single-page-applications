fa8c416f47abef47880bd6848bc440568f5425e6
MalHibou <swperrin51@gmail.com>
2021-01-29 11:53:52 -0600

    add styles to confirmation page

--- src/Confirmation.js
+++ src/Confirmation.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Confirmation (props) {
   return (
-    <div>
+    <div style={{marginLeft:'10%'}}>
       <h2>Congrats! Your pizza is on its way!</h2>
       <h3>Your order:</h3>
       <ul>
         {Object.keys(props.values).map(value => props.values[value] ? <li key={value}>{value}: {props.values[value]}</li> : null)}
       </ul>
-      <Link to='/' onClick={props.resetFormValues}>
-        Go back home
-      </Link>
-      <Link to='/pizza' onClick={props.resetFormValues}>
-        Make another order
-      </Link>
+      <div>
+        <Link to='/' onClick={props.resetFormValues} style={{margin:'5%'}}>
+          Go back home
+        </Link>
+        <Link to='/pizza' onClick={props.resetFormValues} style={{margin:'5%'}}>
+          Make another order
+        </Link>
+      </div>
     </div>
   )
 }
